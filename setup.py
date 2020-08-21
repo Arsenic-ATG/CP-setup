6a11c86b8adec0f32988e621da3969651208e1b7
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-21 18:06:50 +0530

    set up basic layout of the project

---
+++ setup.py
@@ -0,0 +1,7 @@
+# automation to open up all the neccesary website/text editor during a competitive programming competition
+
+import request,webbrowser,sys
+
+# TODO: find the url of the website (code forces)
+# TODO: open the webbrower
+# TODO: open a .cpp file with text editor (sublime text)
