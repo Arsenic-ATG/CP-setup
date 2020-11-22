986f06f801997f236adf576002f484558f3edcf4
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-11-22 16:29:29 +0530

    open a file in default text editor

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 # automation to open up all the neccesary website/text editor during a competitive programming competition
 
-import webbrowser
+import webbrowser,os
 
 # find the url of the website (code forces) and open it in webbrowser
 webbrowser.open("https://codeforces.com/problemset?tags=900-900")
 
-# TODO: open a .cpp file with text editor (sublime text)
+# open a .cpp file with text editor (sublime text)
+os.popen('open test.cpp')
