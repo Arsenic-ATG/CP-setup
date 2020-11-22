038501d7f2b25096a567c758b29709406dda66bd
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-11-22 17:10:27 +0530

    remove the extra conditional statment

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 # automation to open up all the neccesary website/text editor during a competitive programming competition
 import webbrowser,os
 
-# find the url of the website (code forces) and open it in webbrowser
+# find the url of the website (code forces) and open it in default webbrowser
 webbrowser.open("https://codeforces.com/problemset?tags=900-900")
 
-# check if the file already exists
-if os.path.isfile('./test.cpp'):
-	print("it does")
-
-else:
-	file = open("test.cpp", "w") 
-	file.write("//let's do this") 
-	file.close() 
-	print("now it does")
+# check if the file already exists, if not then create one
+file = open("test.cpp", "w") 
+file.write("//cpp code goes here") 
+file.close()
 
 # open the .cpp file with the system's default text editor (sublime text)
 os.system('open test.cpp')
