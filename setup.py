9a31c98d4d93919edaf42b2f1edfacefc5f66d65
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-11-22 16:44:29 +0530

    check if the file exists

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 # automation to open up all the neccesary website/text editor during a competitive programming competition
-
 import webbrowser,os
 
 # find the url of the website (code forces) and open it in webbrowser
 webbrowser.open("https://codeforces.com/problemset?tags=900-900")
 
-# open a .cpp file with text editor (sublime text)
-os.popen('open test.cpp')
+# check if the file already exists
+if os.path.isfile('./test.cpp'):
+	print("it does")
+
+else:
+	file = open("test.cpp", "w") 
+	file.write("//let's do this") 
+	file.close() 
+	print("now it does")
+
+# open the .cpp file with the system's default text editor (sublime text)
+os.system('open test.cpp')
