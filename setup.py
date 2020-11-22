d8d3c2944de8b84344bdd2c80d134f9273424f66
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-11-22 17:40:32 +0530

    add OS independency

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 # automation to open up all the neccesary website/text editor during a competitive programming competition
-import webbrowser,os
+
+# TODO : Ask the user the name of the cpp file while whill be created as command line arguments
+# TODO : currently I am assumning the file would be in current directory only will have to change it afterwords
+
+
+import webbrowser,os,sys,subprocess
 
 # find the url of the website (code forces) and open it in default webbrowser
 webbrowser.open("https://codeforces.com/problemset?tags=900-900")
 
 # check if the file already exists, if not then create one
-file = open("test.cpp", "w") 
+file = open('test.cpp')
 file.write("//cpp code goes here") 
 file.close()
 
-# open the .cpp file with the system's default text editor (sublime text)
-os.system('open test.cpp')
+if sys.platform=='win32':
+    os.startfile("test.cpp")
+
+else:
+    try:
+        os.system("open test.cpp")
+    except OSError:
