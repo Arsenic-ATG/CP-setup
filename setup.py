d7afe16b34f1cff0e8bac424148bec9024b20b1b
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-11-24 17:58:42 +0530

    fix: cpp file not opening

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 # automation to open up all the neccesary website/text editor during a competitive programming competition
+import webbrowser,os
 
-# TODO : Ask the user the name of the cpp file while whill be created as command line arguments
-# TODO : currently I am assumning the file would be in current directory only will have to change it afterwords
-
-
-import webbrowser,os,sys,subprocess
-
-# find the url of the website (code forces) and open it in default webbrowser
+# find the url of the website (code forces) and open it in webbrowser
 webbrowser.open("https://codeforces.com/problemset?tags=900-900")
 
-# check if the file already exists, if not then create one
-file = open('test.cpp')
-file.write("//cpp code goes here") 
-file.close()
-
-if sys.platform=='win32':
-    os.startfile("test.cpp")
+# check if the file already exists
+if os.path.isfile('./test.cpp'):
+	print("it does")
 
 else:
-    try:
-        os.system("open test.cpp")
-    except OSError:
-    	raise Exception("Sorry, The program doesn't support your operating system right now.")
+	file = open("test.cpp", "w") 
+	file.write("//let's do this") 
+	file.close() 
+	print("now it does")
+
+# open the .cpp file with the system's default text editor (sublime text)
+os.system('open test.cpp')
