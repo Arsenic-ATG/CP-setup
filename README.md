2aa8f8c8884eec01b1e028cdf7cc641d7759942e
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-12-10 18:54:07 +0530

    Create a customized version of the repo
    add a new branch to work with a automation customaised for me

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# CP-setup { 👷‍♂️Under construction }
-Automation to make my pc ready for competitive programming
-
-<!--- how to get it running --->
-<!--- prerequisits --->
-
-## License
-
-[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
-
-- **[MIT license](http://opensource.org/licenses/mit-license.php)**
-- Copyright 2015 © <a href="https://github.com/Arsenic-ATG" target="_blank">Ankur Saini</a>.
+# this version is customised to work specifically on Arsenic's machine, if you are looking for a more generalised approach then check out [master branch](https://github.com/Arsenic-ATG/CP-setup/tree/master)
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-# automation to get my pc ready for a competitve programming cometition 
+# this version is customised to work specifically on Arsenic's machine, if you are looking for a more generalised approach then check out master branch
 import webbrowser,os
 
 # TODO : Ask the user the name of the cpp file while whill be created as command line arguments
 # TODO : currently I am assumning the file would be in current directory only will have to change it afterwords
 
-# find the url of the website (code forces) and open it in webbrowser
-# webbrowser.open("https://codeforces.com/problemset?tags=900-900")
+# find the url of the website (code forces problem set page) and open it in webbrowser
+ webbrowser.open("https://codeforces.com/problemset?tags=900-900")
 
 # this is the place where your file will be saved, change if you want to save it somewhere else
 pathStr = '../cp/test.cpp'
