4f7bd0ab37976c872198ea24e6584f60ae966e25
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-12-10 18:24:49 +0530

    Alter target cpp file path
    now the new file will be created at '../cp/test.cpp' instead of the current repo

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-# automation to open up all the neccesary website/text editor during a competitive programming competition
+# automation to get my pc ready for a competitve programming cometition 
 import webbrowser,os
 
 # TODO : Ask the user the name of the cpp file while whill be created as command line arguments
 # TODO : currently I am assumning the file would be in current directory only will have to change it afterwords
 
 # find the url of the website (code forces) and open it in webbrowser
-webbrowser.open("https://codeforces.com/problemset?tags=900-900")
+# webbrowser.open("https://codeforces.com/problemset?tags=900-900")
+
+# this is the place where your file will be saved, change if you want to save it somewhere else
+pathStr = '../cp/test.cpp'
 
 # check if the file already exists
-if !os.path.isfile('./test.cpp'):
-	file = open("test.cpp", "w") 
+if os.path.exists(pathStr) == False:
+	file = open(pathStr, "w") 
+	# add all the initial boiler plate in the file already
 	file.write("//let's do this") 
 	file.close() 
 
 # open the .cpp file with the system's default text editor (sublime text)
-os.system('open test.cpp')
+os.system('open ' + pathStr)
