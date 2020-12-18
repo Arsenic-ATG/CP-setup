e8a116090b746deb6551899354e9a575d122de34
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-12-18 19:52:56 +0530

    Update reference.cpp
    add function to take input to vector, and add few required headers

--- reference.cpp
+++ reference.cpp
@@ -1,8 +1,28 @@
 // There will be more added to this file in the future
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+// to take input from a vector
+void input(std::vector<int> &v,int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		int ele;
+		std::cin>>ele;
+		v.push_back(ele);
+	}
+}
+
 
 int main()
 {
-	// code goes here
+	int T;
+	std::cin>>T;
+	while(T--)
+	{
+		// solution goes here
+	}
 	return 0;
 }
