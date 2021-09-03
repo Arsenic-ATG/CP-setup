02a656822bb8e11dcf82fd902b5ca0dcb6c66b92
Girik Bajaj <bajajgirik2010@gmail.com>
2021-09-03 20:16:01 +0530

    Fixed std using squash

--- reference.cpp
+++ reference.cpp
@@ -4,23 +4,23 @@
 #include <cmath>
 #include <vector>
 
+using namespace std;
 // to take input from a vector
-void input(std::vector<int> &v,int n)
+void input(vector<int> &v, int n)
 {
 	for (int i = 0; i < n; ++i)
 	{
 		int ele;
-		std::cin>>ele;
+		cin >> ele;
 		v.push_back(ele);
 	}
 }
 
-
 int main()
 {
 	int T;
-	std::cin>>T;
-	while(T--)
+	cin >> T;
+	while (T--)
 	{
 		// solution goes here
 	}
